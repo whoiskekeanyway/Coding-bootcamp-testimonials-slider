d0e66bf2cd0f5d978053d20ec8c7a8a15b05b954
whoiskekeanyway <whoiskekeanyway@outlook.com>
2021-12-31 18:14:38 +0200

    new-update

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
+// Carousel container
 const track = document.querySelector(".carousel-track");
+// store all tracks in  the carousel in an array 
 const slides = Array.from(track.children);
+// Bring in next and previous buttons
 const nextButton = document.querySelector(".carousel--button-right");
 const prevButton = document.querySelector(".carousel--button-left");
+ const currentSlide = track.querySelector(".current-slide");
 
-console.log(nextButton);
 
+//  Storing the size of the carousel width in a variable
 const slideWidth = slides[0].getBoundingClientRect().width;
 
+
+//  Setting the initial position of each slide
 const setSlidePosition = (slide, index) => {
   slide.style.left = slideWidth * index + "px";
 };
+// Setting the position for each slide in the carousel woth forEach method 
 slides.forEach(setSlidePosition);
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-  currentSlide.classList.remove("current-slide");
-  targetSlide.classList.add("current-slide");
-};
 
+//  When i click the prevButton, the current slide will move to the left
 prevButton.addEventListener("click", (e) => {
   const currentSlide = track.querySelector(".current-slide");
   const prevSlide = currentSlide.previousElementSibling;
-
   moveToSlide(track, currentSlide, prevSlide);
 });
 
-// when i click right, move slide to the Right
+// When i click the nextButton, the current slide will move to the right
 nextButton.addEventListener("click", (e) => {
   const currentSlide = track.querySelector(".current-slide");
   const nextSlide = currentSlide.nextElementSibling;
   moveToSlide(track, currentSlide, nextSlide);
 });
+
+// 
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
